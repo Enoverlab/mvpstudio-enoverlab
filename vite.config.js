0de174029f526c2b78f2a581c78ad34e7aa01767
BENEDYCT <emekaossy6779@gmail.com>
2025-08-22 00:38:12 +0100

    build config

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,4 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react-vendor';
-            if (id.includes('firebase')) return 'firebase-vendor';
-            if (id.includes('emailjs')) return 'emailjs-vendor';
-            return 'vendor';
-          }
-        }
-      }
-    }
-  }
 })
